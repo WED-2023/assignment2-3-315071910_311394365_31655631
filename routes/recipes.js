7155bb5b066dc49ad81b818e31c659db7304b14c
avramit5674 <150363589+avramit5674@users.noreply.github.com>
2024-07-13 18:24:18 +0300

    Update recipes.js
    add /search

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,8 +5,25 @@ const recipes_utils = require("./utils/recipes_utils.js");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
-router.get("/", (req, res) => res.send("im here"));
+router.get("/", (req, res) => res.send("im here hi"));
 
+
+router.get("/search", async (req, res, next) => {
+  try {
+    console.log("search")
+    const user_id = req.session.user_id;
+    if (!req.query.query) {
+      throw { status: 400, message: "query is missing"};
+    }
+    let search_recipes = await recipes_utils.getSearchRecipes(req.query.query, req.query.number, req.query.cuisine, req.query.diet, req.query.intolerance);
+    if (user_id){
+      req.session.lastSearch = search_recipes;
+    }
+    res.send(search_recipes);
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/random", async (req, res, next) => {
   try {
@@ -36,22 +53,24 @@ router.get("/:recipeId", async (req, res, next) => {
 });
 
 
+
+module.exports = router;
 /**
  * This path is for searching a recipe
  */
-router.get("/search", async (req, res, next) => {
-  try {
-    const recipeName = req.query.recipeName;
-    const cuisine = req.query.cuisine;
-    const diet = req.query.diet;
-    const intolerance = req.query.intolerance;
-    const number = req.query.number || 5;
-    const results = await recipes_utils.searchRecipe(recipeName, cuisine, diet, intolerance, number);
-    res.send(results);
-  } catch (error) {
-    next(error);
-  }
-});
+// router.get("/search", async (req, res, next) => {
+//   try {
+//     const recipeName = req.query.recipeName;
+//     const cuisine = req.query.cuisine;
+//     const diet = req.query.diet;
+//     const intolerance = req.query.intolerance;
+//     const number = req.query.number || 5;
+//     const results = await recipes_utils.searchRecipe(recipeName, cuisine, diet, intolerance, number);
+//     res.send(results);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // /**
 //  * This path returns a full details of a recipe by its id
@@ -65,4 +84,4 @@ router.get("/search", async (req, res, next) => {
 //   }
 // });
 
-module.exports = router;
+
